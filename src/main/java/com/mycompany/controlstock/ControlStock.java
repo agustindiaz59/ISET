967cd07f29428e26a0d18318e53deb95b00ba44a
agustindiaz59 <matutias10000@gmail.com>
2023-11-29 01:11:18 -0300

    1.1

--- src/main/java/com/mycompany/controlstock/ControlStock.java
+++ src/main/java/com/mycompany/controlstock/ControlStock.java
@@ -1,16 +1,8 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
-
 package com.mycompany.controlstock;
 
 import com.mycompany.controlstock.graficos.Principal;
 import java.awt.EventQueue;
 
-/**
- *
- * @author matia
- */
 public class ControlStock {
 
     public static void main(String[] args) {
--- src/main/java/com/mycompany/controlstock/modelo/Producto.java
+++ src/main/java/com/mycompany/controlstock/modelo/Producto.java
@@ -1,17 +1,9 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.mycompany.controlstock.modelo;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-/**
- *
- * @author matia
- */
 @AllArgsConstructor@Data@NoArgsConstructor
 public class Producto {
     private String codigo;
